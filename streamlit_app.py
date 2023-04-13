70b4d4ac5005f0203d54979f1a9450c48128d4fa
Carlos Aranda <104809583+CarlosArandaMelgar@users.noreply.github.com>
2023-04-13 18:45:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-Try
+try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit would you like information")
