29b35f553b9ebad620e11e2cf90b80726651c773
Carlos Aranda <104809583+CarlosArandaMelgar@users.noreply.github.com>
2023-04-13 17:53:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import requeriments.snowflake.connector
+from requeriments import snowflake.connector
 
 streamlit.title('My parents new healthy diner')
 streamlit.text('Prove Text')
