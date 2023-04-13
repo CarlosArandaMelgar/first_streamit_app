669036f5fd4b4316ab636b276f44e2fa90513045
Carlos Aranda <104809583+CarlosArandaMelgar@users.noreply.github.com>
2023-04-13 09:58:49 -0500

    Update streamlit_app.py
    added breaskfast menu, header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 streamlit.text('Prove Text')
+streamlit.header('Menú de desayuno')
+streamlit.text('Omega 3 y avena con arándanos')
+streamlit.text('Batido de col rizada, espinacas y rúcula')
+streamlit.text('Huevo de gallinas camperas hervidas')
