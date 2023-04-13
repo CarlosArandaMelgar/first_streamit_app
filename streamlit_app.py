b8b556c3ac70948eb9eccf95c2b1379e91ada782
Carlos Aranda <104809583+CarlosArandaMelgar@users.noreply.github.com>
2023-04-13 10:03:35 -0500

    Update streamlit_app.py
    added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.text('Prove Text')
 streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo de gallinas camperas hervidas')
+streamlit.text('🥣 Omega 3 y avena con arándanos')
+streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
+streamlit.text('🐔 Huevo de gallinas camperas hervidas') 
+
+streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
