5a165657a8903a381b7000d4a8e719a6c84c366f
Carlos Aranda <104809583+CarlosArandaMelgar@users.noreply.github.com>
2023-04-13 10:10:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ streamlit.text('🐔 Huevo de gallinas camperas hervidas')
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(mi_lista_de_frutas)
+streamlit.dataframe(my_fruit_list)
